61a6e41079083924cc68d71bf09c05c87adba978
Baltenko Mark <markbaltenko@gmail.com>
2024-03-07 23:05:05 +0200

    add app component srtucture.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './pages/feed/feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: FeedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { UserInfoComponent } from './layouts/user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    SidebarComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
