21299e700a3cb75dfa542d8d233367318a51c923
Baltenko Mark <markbaltenko@gmail.com>
2024-03-08 16:18:14 +0200

    fix routing. add status to icons from router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './pages/feed/feed.component';
+import { ExploreComponent } from './pages/explore/explore.component';
 
 const routes: Routes = [
-  { path: "", component: FeedComponent }
+  { path: "", component: FeedComponent },
+  { path: "explore", component: ExploreComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
-import { UserInfoComponent } from './layouts/user-info/user-info.component';
 import { CreateComponent } from './components/common/icons/create/create.component';
 import { ExploreIconComponent } from './components/common/icons/explore-icon/explore-icon.component';
 import { HomeIconComponent } from './components/common/icons/home-icon/home-icon.component';
+import { ExploreComponent } from './pages/explore/explore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
     SidebarComponent,
-    UserInfoComponent,
     CreateComponent,
     ExploreIconComponent,
-    HomeIconComponent
+    HomeIconComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
