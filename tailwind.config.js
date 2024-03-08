94129ca5297854f9914b39250db43e74251fd888
Baltenko Mark <markbaltenko@gmail.com>
2024-03-08 14:42:40 +0200

    add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts,svg}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-seporator": "rgb(38, 38, 38)",
+      },
+      screens: {
+        "lg+": "1263px"
+      }
+
+    },
   },
   plugins: [],
 }
