0815498a78d36a1920b52aa856755759a1f6eb8c
Baltenko Mark <markbaltenko@gmail.com>
2024-03-08 14:43:59 +0200

    add icons for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 import { UserInfoComponent } from './layouts/user-info/user-info.component';
+import { CreateComponent } from './components/common/icons/create/create.component';
+import { ExploreIconComponent } from './components/common/icons/explore-icon/explore-icon.component';
+import { HomeIconComponent } from './components/common/icons/home-icon/home-icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
     SidebarComponent,
-    UserInfoComponent
+    UserInfoComponent,
+    CreateComponent,
+    ExploreIconComponent,
+    HomeIconComponent
   ],
   imports: [
     BrowserModule,
